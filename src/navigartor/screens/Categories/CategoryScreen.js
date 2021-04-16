6515f44b44d7087e0511472bcd352cf6057298bf
Reyes Telesforo Alan Mauricio <al221910354@gmail.com>
2021-04-15 23:02:18 -0500

    'diamondapp'

--- src/navigartor/screens/Categories/CategoryScreen.js
+++ src/navigartor/screens/Categories/CategoryScreen.js
@@ -1,22 +1,50 @@
 import * as React from 'react';
+import { useState } from 'react'
 import { StatusBar } from "expo-status-bar";
-import {Text, StyleSheet,View, Image} from 'react-native';
+import {Text, StyleSheet,View, Image, FlatList} from 'react-native';
 
 export default function CategoryScreen(){
+  const [images, setimages] = useState([
+    require('../../../assets/images/collar.jpg'),
+    require('../../../assets/images/collar1.jpg'),
+    require('../../../assets/images/collar111.jpg'),
+    require('../../../assets/images/reloj2.jpg'),
+    require('../../../assets/images/relog11.jpg'),
+    require('../../../assets/images/reloj3.jpg'),
+    require('../../../assets/images/collar.jpg'),
+    require('../../../assets/images/collar.jpg'),
+    require('../../../assets/images/collar.jpg')
+  ]);
      return (
-    <View style={styles.container}>
-     
-        <StatusBar style="auto" />
-         
-        <Text>....................</Text>
- 
+    <FlatList
+    data={images}
+    key={"2"}
+    numColumns={2}
+    renderItem={({ item }) => (
+      <View style={styles.container}>
         <Image
-          style={{ width: 100, height: 100 }}
-          source={{ uri: "https://reactjs.org/logo-og.png" }}
+          source={item}
+          style={{
+            width: 170,
+            height: 200,
+            borderWidth: 2,
+            borderColor: "#212F3C",
+            resizeMode: "contain",
+            margin: 5,
+          }}
+          keyExtractor={(item) => item.id}
         />
-       
-    </View>
+        
+      </View>
+    )}
+  />
   );
 }
 
 
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    padding: 1,
+    },
+    });
